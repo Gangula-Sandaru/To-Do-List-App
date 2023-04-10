495801685581381af3611da10039d0816a216833
Gangula Sandaru Dinusantha <gangula.sandaru@gmail.com>
2023-04-10 14:58:34 +0530

    finished the Database helper file

--- app/src/main/java/com/gangula/todolist/DatabaseHelper.java
+++ app/src/main/java/com/gangula/todolist/DatabaseHelper.java
@@ -1,4 +1,84 @@
 package com.gangula.todolist;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class DatabaseHelper {
+import androidx.annotation.Nullable;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.net.ConnectException;
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "students.db";
+    public static final String TABLE_NAME= "student_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "COURSE";
+    public static final String COL_4 = "MARK";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context,DATABASE_NAME, null, 1);
+
+        // created a object  for created the database
+        SQLiteDatabase db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        // SQL query for the database
+        sqLiteDatabase.execSQL("create table " + TABLE_NAME + "(ID INTEGER primary key autoincrement, " + "NAME TEXT, COURSE TEXT, MARK INTERGER)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+
+    }
+
+    public boolean insertData(String name, String course, String marks){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, course);
+        contentValues.put(COL_4, marks);
+
+        long result  = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor result = db.rawQuery("select * from " + TABLE_NAME , null);
+        return result;
+
+    }
+
+    public boolean updateData(String id, String name, String course, String marks){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, course);
+        contentValues.put(COL_4, marks);
+        db.update(TABLE_NAME, contentValues, "id = ? ", new String[] {id});
+        return  true;
+    }
+
+
+    public Integer deleteData(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?", new String[] {id});
+
+    }
 }
